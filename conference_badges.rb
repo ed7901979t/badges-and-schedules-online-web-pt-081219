2b4ddd264e02a5bb24a7950119b18bb0aef987e1
clean-command-1851 <ed7901979t@gmail.com>
2019-08-15 18:55:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@
 guests = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-  return "Hello, my name is #{name}"
+  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
