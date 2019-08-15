f1cbf206205a39122029e5b9220e73d034ed4f1c
clean-command-1851 <ed7901979t@gmail.com>
2019-08-15 18:02:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,9 +8,11 @@ def batch_badge_creator(arr)
   #returns an array of badge messages
   arr.each {|n| puts "Hello, #{n}!"
   
-def assign_rooms
+def assign_rooms(arr)
   #takes list of speakers and assigns a room only one room
   #1-7 return list of rooms as a message...
+  room_numbers[1,2,3,4,5,6,7]
+  arr.each_with_index {|n| |index|=room_numbers.index }
   
 def printer
   #output first results of batch_badge_creator
