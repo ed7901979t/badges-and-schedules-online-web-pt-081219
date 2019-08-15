aa8c3d85beb15ff09ebf4f85a8f839f9c92a5207
clean-command-1851 <ed7901979t@gmail.com>
2019-08-15 17:50:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,11 @@ end
 def batch_badge_creator
   #takes an array of names as argument
   #returns an array of badge messages
+  
+def assign_rooms
+  #takes list of speakers and assigns a room only one room
+  #1-7 return list of rooms as a message...
+  
+def printer
+  #output first results of batch_badge_creator
+  #assign_rooms iterate for puts
